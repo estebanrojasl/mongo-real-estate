c08126adf22dbcb16da3259753bd758631979602
esteban7590 <esteban7590@hotmail.com>
2020-12-28 17:19:56 -0500

    testing err responses

--- config/production.js
+++ config/production.js
@@ -1,6 +1,4 @@
 const config = {
   Port: process.env.PORT,
-  CnxString:
-    "mongodb+srv://admin:gKLAfxQPWYPVBfFH@cluster0.sjink.mongodb.net/real-estate?retryWrites=true&w=majority",
 };
 module.exports = { config };
--- src/db/mongodb.js
+++ src/db/mongodb.js
@@ -3,7 +3,7 @@ const ENV = process.env.NODE_ENV || "development";
 const config = require("../../config/" + ENV).config;
 
 mongoose
-  .connect(process.env.MONGODB_URI, {
+  .connect(process.env.MONGODB_URI || config.CnxString, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
   })
