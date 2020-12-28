b0d8c1a3967f2122d9817f59017856a949c5c650
esteban7590 <esteban7590@hotmail.com>
2020-12-28 16:09:53 -0500

    chgange port config

--- config/production.js
+++ config/production.js
@@ -1,5 +1,5 @@
 const config = {
-  Port: process.env.PORT,
+  Port: process.env.PORT || 5500,
   CnxString:
     "mongodb+srv://admin:gKLAfxQPWYPVBfFH@cluster0.sjink.mongodb.net/real-estate?retryWrites=true&w=majority",
 };
