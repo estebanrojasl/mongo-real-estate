974405343c886b631063a0a226fcaf3a9ae08a6b
esteban7590 <esteban7590@hotmail.com>
2020-12-28 15:13:51 -0500

    updated folder structure

---
+++ config/development.js
@@ -0,0 +1,6 @@
+const config = {
+  Port: process.env.PORT || 5500,
+  CnxString:
+    "mongodb+srv://admin:gKLAfxQPWYPVBfFH@cluster0.sjink.mongodb.net/real-estate?retryWrites=true&w=majority",
+};
+module.exports = { config };
